b4f87216fcb366b5af1c527a82ae0426c1035685
SamuelsonH2T <samu.elsonh2t@gmail.com>
2017-03-09 22:55:12 -0300

    exemplo familia

---
+++ familia.pl
@@ -0,0 +1,21 @@
+gerou(kely, maria).
+gerou(cleber, maria).
+gerou(cleber, bruna).
+gerou(maria, julia).
+gerou(maria, pedro).
+gerou(pedro, lucas).
+
+feminino(kely).
+feminino(maria).
+feminino(julia).
+feminino(bruna).
+
+masculino(cleber).
+masculino(pedro).
+masculino(lucas).
+
+
+%consultas
+
+avo(kely, julia)
+avo(kely,pedro)
